851e11e356b77756009b594215798a4bad0474cc
huynhthu214 <minhthuhuynh23@gmail.com>
2025-04-03 07:54:32 +0700

    cd

---
+++ Template_Report/demo/view/footer.php
@@ -0,0 +1,30 @@
+<footer class="py-4 mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h5>🍃TeaV</h5>
+            <p>
+              Connecting tea lovers with global traditions, we explore the art
+              of brewing and the rich stories behind every carefully selected
+              leaf.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h5>Contact</h5>
+            <p>Email: trangtra@gmail.com</p>
+            <p>Phone: +123 456 7890</p>
+          </div>
+          <div class="col-md-4">
+            <h5>Follow Us</h5>
+            <a href="#" class="text-light me-2">Facebook</a>
+            <a href="#" class="text-light me-2">Instagram</a>
+            <a href="#" class="text-light">YouTube</a>
+          </div>
+        </div>
+        <div class="text-center mt-3">
+          <p class="mb-0">
+            &copy; <span id="year"></span> 2025 Trang Tra. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
