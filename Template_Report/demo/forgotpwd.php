6c4c9ad63963d4f38542abef971dff983518ab21
huynhthu214 <minhthuhuynh23@gmail.com>
2025-04-09 16:37:33 +0700

    sql

--- Template_Report/demo/forgotpwd.php
+++ Template_Report/demo/forgotpwd.php
@@ -3,6 +3,27 @@
     $namePage = "Forgot Password";
     include "view/header.php";
 ?>
+<div class="container mt-5 d-flex justify-content-center">
+  <div class="form-box p-5" style="width: 450px; background-color: #ffffff;">
+    <h1 class="text-center mb-4 sign-up">Forgot Password</h1>
+    <form>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          placeholder="Enter your email"
+          required
+        />
+      </div>
+      <button type="submit" class="btn btn-primary w-100 signup-btn">Continue</button>
+      <div class="text-center mt-3">
+        <a href="login.php" class="text-decoration-none">Back to login</a>
+      </div>
+    </form>
+  </div>
+</div>
 <?php 
     include "view/footer.php";
 ?>
--- Template_Report/demo/login.php
+++ Template_Report/demo/login.php
@@ -4,27 +4,33 @@
     include "view/header.php";
 ?>
 <!-- Login -->
+<div class="center-container">
+  <div class="form-box">
     <form method="post">
-        <div class="mt-5">
-          <h1 class="login">Login</h1>
-          <label for="email" class="form-label">Email</label>
-          <input type="email" class="form-control" id="email" placeholder="Enter your email" />
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" placeholder="Enter your password" />
-        </div>
-        <!-- Forgot Password Link -->
-        <div class="mb-3 text-end">
-          <a href="forgotpwd.php" id="forgotPassword" class="text-decoration-none">Forgot Password?</a>
-        </div>
-        <button type="submit" class="btn btn-primary login-btn">Login</button>
-        <div class="text-center mt-3">
-          <p>
-            Don't have an account? <a href="signup.php" id="openRegisterModal">Sign Up</a>
-          </p>
-        </div>
-      </form>
+      <h1 class="login text-center mb-4">Login</h1>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter your email" />
+      </div>
+
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter your password" />
+      </div>
+
+      <div class="mb-3 text-end">
+        <a href="forgotpwd.php" id="forgotPassword" class="text-decoration-none">Forgot Password?</a>
+      </div>
+
+      <button type="submit" class="btn btn-primary login-btn">Login</button>
+
+      <div class="text-center mt-3">
+        <p>Don't have an account? <a href="signup.php" id="openRegisterModal">Sign Up</a></p>
+      </div>
+    </form>
+  </div>
+</div>
 
 <?php 
     include "view/footer.php";
