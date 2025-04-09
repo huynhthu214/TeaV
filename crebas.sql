04e4266ab7ec00d34355f8f63a09736cc2804ede
huynhthu214 <minhthuhuynh23@gmail.com>
2025-04-09 16:56:34 +0700

    add scrtipt sql

--- crebas.sql
+++ crebas.sql
@@ -1,10 +1,22 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     4/9/2025 4:33:03 PM                          */
+/* Created on:     4/9/2025 4:55:38 PM                          */
 /*==============================================================*/
 
 
+drop table if exists Account;
+
 drop table if exists Product;
+
+/*==============================================================*/
+/* Table: Account                                               */
+/*==============================================================*/
+create table Account
+(
+   Usename              varchar(50) not null  comment '',
+   Password             varchar(20)  comment '',
+   primary key (Usename)
+);
 
 /*==============================================================*/
 /* Table: Product                                               */
